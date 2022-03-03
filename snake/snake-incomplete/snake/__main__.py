fef6e3d83e6fe8a237ca7ec31b2b2d828c75ab13
shauniebyrne <byr21002@byui.edu>
2022-03-03 12:21:52 -0700

    team changes

--- snake/snake-incomplete/snake/__main__.py
+++ snake/snake-incomplete/snake/__main__.py
@@ -17,11 +17,11 @@ from game.shared.point import Point
 
 
 def main():
-    
+
     # create the cast
     cast = Cast()
-    cast.add_actor("foods", Food())
-    cast.add_actor("snakes", Snake())
+    cast.add_actor("snakes", Snake(1))
+    cast.add_actor("snakes", Snake(2))
     cast.add_actor("scores", Score())
    
     # start the game
